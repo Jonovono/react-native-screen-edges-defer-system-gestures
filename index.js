34e3715131f1588530b758f7b39c6ac3157e57d9
Jordan Howlett <jordan@howlett.io>
2021-11-25 08:44:00 -0500

    Clean up

--- index.js
+++ index.js
@@ -2,35 +2,22 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { NativeModules, Platform } from "react-native";
 
-const { RNHomeIndicator } = NativeModules;
+const { RNDeferScreenEdges } = NativeModules;
 const isIos = Platform.OS === "ios";
 
 const propTypes = {
     autoHidden: PropTypes.bool.isRequired,
 };
 
-export class HomeIndicator extends Component {
-    static propsHistory = [];
+export class DeferScreenEdgesView extends Component {
 
-    static popAndGetPreviousProps() {
-        HomeIndicator.propsHistory.pop();
-        return HomeIndicator.propsHistory[HomeIndicator.propsHistory.length - 1] || {};
-    }
 
     componentDidMount() {
         if (!isIos) return;
-
-        const { autoHidden } = this.props;
-        HomeIndicator.propsHistory.push(this.props);
-
-        updateNativeHomeIndicator({ autoHidden });
     }
 
     componentWillUnmount() {
         if (!isIos) return;
-
-        const { autoHidden } = HomeIndicator.popAndGetPreviousProps();
-        updateNativeHomeIndicator({ autoHidden });
     }
 
     render() {
@@ -49,5 +36,5 @@ function updateNativeHomeIndicator({ autoHidden = false }) {
 }
 
 // keep this for backwards compatibility
-const PrefersHomeIndicatorAutoHidden = () => <HomeIndicator autoHidden />;
+const PreferresScreenEdgesDeferringSystemGestures = () => <DeferScreenEdgesView />;
 export default PrefersHomeIndicatorAutoHidden;
